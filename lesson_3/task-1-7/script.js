76eb775435f8ce20d37930f8aa86a2ef1390512b
hmuryi-koot <hmuryikoot@gmail.com>
2023-02-15 23:25:39 +0300

    Fix lesson 3 task 7

--- lesson_3/task-1-7/script.js
+++ lesson_3/task-1-7/script.js
@@ -8,8 +8,8 @@ const monthNumber = parseInt(prompt('Введите номер дня недел
 
 document.write(`${monthNumber} месяц - `)
 
-if (monthNumber === 12 || monthNumber >= 1 && monthNumber <= 2) document.write('зима')
-else if (monthNumber >= 3 && monthNumber <= 5) document.write('весна')
-else if (monthNumber >= 6 && monthNumber <= 8) document.write('лето')
-else if (monthNumber >= 9 && monthNumber <= 11) document.write('осень')
-else document.write('некорректное значение')
+if (monthNumber > 12 || monthNumber < 1 || isNaN(monthNumber)) document.write('некорректное значение')
+else if (monthNumber === 12 || monthNumber >= 1 && monthNumber <= 2) document.write('зима')
+else if (monthNumber <= 5) document.write('весна')
+else if (monthNumber <= 8) document.write('лето')
+else if (monthNumber <= 11) document.write('осень')
